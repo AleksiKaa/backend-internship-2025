18ea34559486000bb6899b0be10d263c86d2e44a
Aleksi Kääriäinen <allu.kaariainen@gmail.com>
2025-01-20 11:28:50 +0200

    clean code

--- src/backend.py
+++ src/backend.py
@@ -1,61 +1,17 @@
+from utils import calculate_price
+
 from fastapi import FastAPI
-import asyncio
 import aiohttp
 
 app = FastAPI()
 
 
-@app.get("/api")
-def read_root():
-    return {"Hello": "World"}
-
-
 @app.get("/api/v1/delivery-order-price")
 async def get(venue_slug: str, cart_value: int, user_lat: float, user_lon: float):
-    results = await get_venue_information(venue_slug)
+    venue_info = await get_venue_information(venue_slug)
     user_coords = [user_lat, user_lon]
-    order_info = calculate_price(results, cart_value, user_coords)
-    return order_info
-
-
-def calculate_distance(user_coords: list[float], venue_coords: list[float]) -> int:
-    """Calculate the straight line distance for two pairs of points"""
-    x1, y1 = user_coords
-    x2, y2 = venue_coords
-
-    return int(((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5)
-
-
-def calculate_delivery_fee(base_price: int, a: int, b: int, distance: float) -> int:
-    """Formula for calculating the delivery fee"""
-    return base_price + a + b * distance // 10
-
-
-def calculate_price(
-    venue_information: dict, cart_value: int, user_coords: list[int]
-) -> {}:
-    """Calculate the price of the order according to assignment specifications"""
-    base_price, order_minimum_no_surcharge, venue_coords = (
-        venue_information["base_price"],
-        venue_information["order_minimum_no_surcharge"],
-        venue_information["coordinates"],
-    )
-
-    dist = calculate_distance(user_coords, venue_coords)
-    for dist_range in venue_information["distance_ranges"]:
-        if dist >= dist_range["min"] and dist < dist_range["max"]:
-            delivery_fee = calculate_delivery_fee(
-                base_price, dist_range["a"], dist_range["b"], dist
-            )
-
-    small_order_surcharge = max(cart_value - order_minimum_no_surcharge, 0)
-    order_info = {
-        "total_price": cart_value + small_order_surcharge + delivery_fee,
-        "small_order_surcharge": small_order_surcharge,
-        "cart_value": cart_value,
-        "delivery": {"fee": delivery_fee, "distance": dist},
-    }
-    return order_info
+    order_price = calculate_price(venue_info, cart_value, user_coords)
+    return order_price
 
 
 async def get_venue_information(venue_slug: str):
---
+++ src/utils.py
@@ -0,0 +1,43 @@
+def calculate_distance(user_coords: list[float], venue_coords: list[float]) -> int:
+    """Calculate the straight line distance for two pairs of points"""
+    x1, y1 = user_coords
+    x2, y2 = venue_coords
+
+    return int(((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5)
+
+
+def calculate_delivery_fee(base_price: int, a: int, b: int, distance: float) -> int:
+    """Formula for calculating the delivery fee"""
+    return base_price + a + b * distance // 10
+
+
+def calculate_price(
+    venue_information: dict, cart_value: int, user_coords: list[int]
+) -> {}:
+    """Calculate the price of the order according to assignment specifications"""
+    base_price, order_minimum_no_surcharge, venue_coords = (
+        venue_information["base_price"],
+        venue_information["order_minimum_no_surcharge"],
+        venue_information["coordinates"],
+    )
+
+    # Loop over ranges to find a suitable one
+    dist = calculate_distance(user_coords, venue_coords)
+    for dist_range in venue_information["distance_ranges"]:
+        if dist >= dist_range["min"] and dist < dist_range["max"]:
+            # Suitable range found, calculate values of importance
+            delivery_fee = calculate_delivery_fee(
+                base_price, dist_range["a"], dist_range["b"], dist
+            )
+
+            small_order_surcharge = max(cart_value - order_minimum_no_surcharge, 0)
+            order_info = {
+                "total_price": cart_value + small_order_surcharge + delivery_fee,
+                "small_order_surcharge": small_order_surcharge,
+                "cart_value": cart_value,
+                "delivery": {"fee": delivery_fee, "distance": dist},
+            }
+            return order_info
+
+    # Handle case where no suitable range was found
+    return {}
